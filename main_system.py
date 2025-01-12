9e2a7aa88d59efc4de36848bebf93787add71635
coencoensmeets <coencoensmeets@gmail.com>
2025-01-12 22:32:38 +0100

    Webserver class frozen in firmware

--- main_system.py
+++ main_system.py
@@ -249,5 +249,5 @@ class main_system():
 		print("Memory free:", gc.mem_free(), "bytes")
 
 if __name__ == '__main__':
-	system = main_system(safety_switch=False)
+	system = main_system(safety_switch=True)
 	system.start_threads()
